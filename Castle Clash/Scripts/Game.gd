f31857b0ada7a1fcc9148f4e878c0f1b185b5226
keklingaiu <70243843+keklingaiu@users.noreply.github.com>
2021-03-21 20:36:44 -0400

    Basic room/path generation
    Creates one giant room with small single block paths, will adjust in next milestone

---
+++ Castle Clash/Scripts/Game.gd
@@ -0,0 +1,49 @@
+extends Node2D
+
+onready var tileMap = $Walls
+
+var borders = Rect2(1, 1, 37, 20)
+var stepsToWalk = 800
+
+#var realTime = OS.get_time()
+
+#var path = "user://time.json"
+
+func _ready():
+	randomize()
+	create_level()
+	
+func create_level():
+	var levelWalker = LevelWalker.new(Vector2(18, 10), borders)
+	var level = levelWalker.walk(stepsToWalk)
+	levelWalker.queue_free()
+	for location in level:
+		tileMap.set_cellv(location, -1)
+	tileMap.update_bitmask_region(borders.position, borders.end)
+	
+#func save_time():
+#	var save_time = File.new()
+#	save_time.open(path, File.WRITE)
+#	var realTime = OS.get_time()
+#	save_time.store_line(to_json(realTime))
+#	save_time.close()
+#
+#func load_time():
+#	var save_time = File.new()
+#	if not save_time.file_exists(path):
+#		return
+#	save_time.open(path, File.READ)
+#	var text = save_time.get_as_text()
+#	print(text)
+#	save_time.close()
+#	return text
+#
+#func do_randomize():
+#	load_time()
+#	randomize()
+	
+	
+	
+
+	
+
---
+++ Castle Clash/Scripts/LevelWalker.gd
@@ -0,0 +1,46 @@
+extends Node
+
+class_name LevelWalker
+
+const moveableDirections = [Vector2.RIGHT, Vector2.UP, Vector2.LEFT, Vector2.DOWN]
+var position = Vector2.ZERO
+var direction = Vector2.RIGHT
+var borderOfMap = Rect2()
+
+var step_history = []
+var steps_since_turn = 0
+
+func _init(start_pos, new_borders):
+	assert(new_borders.has_point(start_pos))
+	position = start_pos
+	step_history.append(position)
+	borderOfMap = new_borders
+	
+func walk(steps):
+	for step in steps:
+		if randf() <= 0.25 or steps_since_turn >= 4:
+			switch_dir()
+			
+		if step():
+			step_history.append(position)
+		else:
+			switch_dir()
+	return step_history
+	
+func step():
+	var target_pos = position + direction
+	if borderOfMap.has_point(target_pos):
+		steps_since_turn += 1
+		position = target_pos
+		return true
+	else:
+		return false
+	
+func switch_dir():
+	steps_since_turn = 0
+	var directions = moveableDirections.duplicate()
+	directions.erase(direction)
+	directions.shuffle()
+	direction = directions.pop_front()
+	while not borderOfMap.has_point(position + direction):
+		direction = directions.pop_front()
